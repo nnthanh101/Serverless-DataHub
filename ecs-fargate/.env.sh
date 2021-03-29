ac2908b6c29458ca6fbc935464cdf475809d6e08
Thanh Nguyen <nnthanh101@gmail.com>
2021-03-29 21:55:58 +0700

    🚀 2. VPC

--- ecs-fargate/.env.sh
+++ ecs-fargate/.env.sh
@@ -2,16 +2,16 @@
 export PROJECT_ID=ecs-fargate
 
 ## 1.1. Configuring AWS
-export AWS_PROFILE=nnthanh
+export AWS_PROFILE=default
 
 ## 1.2. AWS Account & Region
 if [ $(uname -s) == 'Darwin' ] ; then
     export AWS_ACCOUNT=$(aws sts get-caller-identity | jq -r '.Account' | tr -d '\n')
-    export AWS_REGION=${AWS_REGION:-"ap-southeast-2"}
+    export AWS_REGION=${AWS_REGION:-"ap-southeast-1"}
 else
     export AWS_ACCOUNT=$(aws sts get-caller-identity --output text --query Account)
     # export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
-    export AWS_REGION=${AWS_REGION:-"ap-southeast-2"}
+    export AWS_REGION=${AWS_REGION:-"ap-southeast-1"}
 fi
 
 ## 2.1. AWS Infra: S3, VPC 
--- ecs-fargate/lib/ecs-fargate-stack.ts
+++ ecs-fargate/lib/ecs-fargate-stack.ts
@@ -1,9 +1,45 @@
 import * as cdk from '@aws-cdk/core';
+import { Config } from '../config/config';
+import { VpcConstruct } from './vpc-construct';
+import { VpcNoNatConstruct } from './vpc-no-nat-construct';
 
+/**
+ * ECS-Fargate
+ * 
+ * VPC0: 0 NAT-Gateway, Public/Isolated Subnets ONLY
+ * VPC1: 2 NAT-Gateway, Public/Private/Isolated Subnets
+ * 
+ * /web        ==> React.js Frontend 1      <-- fargateAutoscalerStack(Connection) + CodePineline 1
+ * /crawl      ==> Crawl Node.js Backend 1  <-- fargateAutoscalerStack(RAM)        + CodePineline 2
+ * /sync       ==> Sync  Node.js Backend 2  <-- fargateAutoscalerStack(RAM)        + CodePineline 2
+ */
 export class EcsFargateStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    /** VpcConstruct: NAT-Gateway >= 1 */
+    const vpc = new VpcConstruct(this, Config.vpcConstructId, {
+      maxAzs:         Config.maxAzs,
+      cidr:           Config.cidr,
+      ports:          Config.publicPorts,
+      natGateways:    Config.natGateways,
+      useDefaultVpc:  Config.useDefaultVpc,
+      vpcId:          Config.vpcId,
+      useExistVpc:    Config.useExistVpc
+    });
+
+    /** VpcNoNatConstruct: NAT-Gateway == 0 */
+    // const vpc = new VpcNoNatConstruct(this, Config.vpcConstructId, {
+    //     maxAzs: Config.maxAzs,
+    //     cidr: Config.cidr,
+    //     ports: Config.publicPorts,
+    //     natGateways: Config.natGateways,
+    //     useDefaultVpc: Config.useDefaultVpc,
+    //     vpcId: Config.vpcId,
+    //     useExistVpc: Config.useExistVpc
+    // }); 
+
   }
 }
