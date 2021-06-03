b512d6fe41ae334b15cfab787575eda4d73edad2
Hieu Le <letrunghieu.cse09@gmail.com>
2021-06-03 23:13:20 +0700

    Install jq for Cloud9

--- cudos-framework/cloud9.sh
+++ cudos-framework/cloud9.sh
@@ -1,4 +1,6 @@
 source "./script/lib/common.sh"
+_logger "[+] Install required packages ..."
+sudo yum install jq -y
 
 _logger "[+] Verify Prerequisites ..."
 echo "[x] Verify AWS CLI:" "$(aws --version 2>&1)"
