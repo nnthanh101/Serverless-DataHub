000118acbee4e4e8a9eb166666c210f1b0f48673
Thanh Nguyen <nnthanh101@gmail.com>
2021-08-04 16:08:59 +0700

    🚀 https://microservices.job4u.vn/   spa-website/develop-cdk

--- cdk/spa-website/lib/spa-website-stack.ts
+++ cdk/spa-website/lib/spa-website-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from '@aws-cdk/core';
+import { SpaWebsite } from 'spa-website';
 
 export class SpaWebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    /** Deploying a Website to AWS S3 */
+    new SpaWebsite(this, 'SPA-Website-S3')
+      .createBasicSite({
+        indexDoc: 'index.html',
+        websiteFolder: '../../../../startup-blueprint'
+      });
+
+    /** Deploying a SPA-Website to AWS S3 behind CloudFront CDN */
+    new SpaWebsite(this, 'SPA-Website-Cloudfront')
+      .createSiteWithCloudfront({
+        indexDoc: 'index.html',
+        websiteFolder: '../../../../admin-dashboard'
+      });
+
   }
 }
