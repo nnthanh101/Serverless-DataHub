6268d0ffb0bcb0d818dc06fa96f691b2b077d837
Thanh Nguyen <nnthanh101@gmail.com>
2021-08-10 20:26:01 +0700

    🎯 projects/spa-website [CI/CD] Github Actions

---
+++ bootstrap.sh
@@ -0,0 +1,67 @@
+#!/bin/bash 
+set -euo pipefail
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+function _logger() {
+    echo -e "$(date) ${YELLOW}[*] $@ ${NC}"
+}
+
+. ./.env
+
+echo
+echo "#########################################################"
+echo "[+] 0. Verify the prerequisites environment"
+echo "#########################################################"
+echo
+
+## DEBUG
+echo "[x] Verify AWS CLI": $(aws  --version)
+echo "[x] Verify git":     $(git  --version)
+echo "[x] Verify jq":      $(jq   --version)
+echo "[x] Verify nano":    $(nano --version)
+echo "[x] Verify Docker":  $(docker version)
+echo "[x] Verify Docker Deamon":  $(docker ps -q)
+# echo "[x] Verify nvm":     $(nvm ls)
+echo "[x] Verify Node.js": $(node --version)
+echo "[x] Verify CDK":     $(cdk  --version)
+echo "[x] Verify Python":  $(python -V)
+echo "[x] Verify Python3": $(python3 -V)
+echo "[x] Verify kubectl":  $(kubectl version --client)
+
+# echo $AWS_ACCOUNT + $AWS_REGION + $AWS_S3_BUCKET
+# currentPrincipalArn=$(aws sts get-caller-identity --query Arn --output text)
+# ## Just in case, you are using an IAM role, we will switch the identity from your STS arn to the underlying role ARN.
+# currentPrincipalArn=$(sed 's/\(sts\)\(.*\)\(assumed-role\)\(.*\)\(\/.*\)/iam\2role\4/' <<< $currentPrincipalArn)
+# echo $currentPrincipalArn
+
+started_time=$(date '+%d/%m/%Y %H:%M:%S')
+echo
+echo "#########################################################"
+echo "[+] [START] Bootstrap CDK at ${started_time}"
+echo "#########################################################"
+echo
+
+echo
+echo "#########################################################"
+echo "[+] Install TypeScript node_modules & CDK Bootstrap ..."
+echo "#########################################################"
+echo
+
+# npm install
+# npm run build
+
+cdk bootstrap aws://${AWS_ACCOUNT}/${AWS_REGION} \
+    --bootstrap-bucket-name ${AWS_S3_BUCKET}     \
+    --termination-protection                     \
+    --tags Cost=cdk
+## cdk bootstrap aws://${AWS_ACCOUNT}/${AWS_REGION} --show-template -v
+
+ended_time=$(date '+%d/%m/%Y %H:%M:%S')
+echo
+echo "#########################################################"
+echo -e "${RED} [END] Bootstrap CDK at ${ended_time} - ${started_time} ${NC}"
+echo "#########################################################"
+echo
