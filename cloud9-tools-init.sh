be0daed56fe7c5a9742f46ef1e4037f6d1629c4b
@nnthanh101 <40840164+nnthanh101@users.noreply.github.com>
2021-12-31 12:07:40 +0700

    Update cloud9-tools-init.sh

--- cloud9-tools-init.sh
+++ cloud9-tools-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+# set -euxo pipefail
 
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
@@ -15,27 +15,28 @@ _logger "-----------------------------------------------------------------------
 _logger "Preparing your environment ..."
 
 date
+# echo "LANG=en_US.utf-8" >> /etc/environment
+# echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 sudo bash -c "echo LANG=en_US.utf-8 >> /etc/environment"
-sudo bash -c " echo LC_ALL=en_US.UTF-8 >> /etc/environment"
+sudo bash -c "echo LC_ALL=en_US.UTF-8 >> /etc/environment"
 
 echo '=== INSTALL and CONFIGURE default software components ==='
 
 sudo yum -y update 
 sudo yum -y remove aws-cli
 
+_logger "Python3.8 >> Lambda runtime compatible !!!"
 # sudo yum install -y amazon-linux-extras
 # amazon-linux-extras | grep -i python
 sudo amazon-linux-extras enable python3.8
 sudo yum install -y python3.8
-echo "alias python='/usr/bin/python3.8'" >> ~/.bashrc
-. ~/.bashrc
 
 ## Because yum does not support Python3
-echo "Replace #!/usr/bin/python by #!/usr/bin/python2.7 in /usr/bin/yum !!!"
+echo "Replace python by python2.7 in /usr/bin/yum !!!"
 sudo sed -i 's/python/python2.7/' /usr/bin/yum
 
-## Nake 2.7 as default python
-echo "sudo ln -sf /usr/bin/python2.7 /usr/bin/python"
+## [Note - yum issue] Nake 2.7 as default python
+# sudo ln -sf /usr/bin/python2.7 /usr/bin/python
 
 # sudo rm /usr/bin/python
 # sudo ln -s /usr/bin/python3.8 /usr/bin/python
@@ -44,7 +45,14 @@ echo "sudo ln -sf /usr/bin/python2.7 /usr/bin/python"
 # sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.8 1
 # sudo update-alternatives --list | grep python
 # alternatives --set python /usr/bin/python3.8
-sudo -H -u ec2-user bash -c "pip install --user -U boto boto3 botocore awscli aws-sam-cli"
+# sudo -H -u ec2-user bash -c "pip install --user -U boto boto3 botocore awscli aws-sam-cli"
+sudo -H -u ec2-user bash -c "pip install --user -U boto boto3 botocore aws-sam-cli"
+
+echo "Installing the AWS CLI version 2 ..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+rm -rf awscliv2.zip aws
 
 ## Check for AWS Region --------------------------
 export AWS_REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
@@ -60,24 +68,24 @@ aws configure set default.region ${AWS_REGION}
 aws configure get default.region
 
 export NVM_VER=$(curl --silent "https://github.com/nvm-sh/nvm/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#') #v0.39.1
-export CDK_VER=$(curl --silent "https://github.com/DevAx101/MicroServices/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#') #v.1.0.1
+export CDK_VER=$(curl --silent "https://github.com/DevAx101/cdk/releases/latest" | sed 's#.*tag/\(.*\)\".*#\1#') #v.1.0.1
 export PACKER_VER=1.7.8
 
 ## Clone CDK and install dependencies ------------
 echo "Cloning CDK Repo ..."
 cd ~/environment
-# git clone --depth 1 --branch $CDK_VER https://github.com/DevAx101/MicroServices.git >/dev/null 2>&1
-git clone https://github.com/DevAx101/MicroServices.git >/dev/null 2>&1
+# git clone --depth 1 --branch $CDK_VER https://github.com/DevAx101/cdk.git >/dev/null 2>&1
+git clone https://github.com/DevAx101/cdk.git >/dev/null 2>&1
 cd $cwd
 echo "Installing dependencies ..."
 sudo yum -y remove aws-cli
 sudo yum install golang jq gettext bash-completion -y -q -e 0 >/dev/null 2>&1
 echo "Enabling utilities scripts ..."
 chmod +x cloud9-ebs-resize.sh
-echo "Resizing AWS Cloud9 Volume ..."
-# ./cloud9-ebs-resize.sh ## 50GB by default
+_logger "Resizing AWS Cloud9 Volume >> 250GB ..."
+./cloud9-ebs-resize.sh 250
 
-## NVM & Node Versions >> Lambda runtime compatible ---------------------------
+_logger "NVM & Node.js 14.x >> Lambda runtime compatible !!!"
 echo "Installing nvm ..."
 rm -rf ~/.nvm
 export NVM_DIR=
@@ -112,10 +120,15 @@ echo '=== PREPARE REBOOT in 1 minute with at ==='
 FILE=$(mktemp) && echo $FILE && echo '#!/bin/bash' > $FILE && echo 'reboot -f --verbose' >> $FILE && at now + 1 minute -f $FILE
 echo "Bootstrap completed with return code $?"
 
+sudo bash -c "echo alias python=/usr/bin/python3.8 >> ~/.bashrc"
+# echo "alias python='/usr/bin/python3.8'" >> ~/.bashrc
+. ~/.bashrc
+
 _logger "[+] Verify Prerequisites ..."
 echo "[x] Verify Git client":        $(git --version)
 echo "[x] Verify jq":                $(jq   --version)
 echo "[x] Verify AWS CLI version 2": $(aws --version)
+echo "[x] Verify AWS SAM":           $(sam --version)
 echo "[x] Verify Node.js":           $(node --version)
 echo "[x] Verify CDK":               $(cdk --version)
 echo "[x] Verify Python":            $(python -V)
